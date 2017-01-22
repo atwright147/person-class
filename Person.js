ea1491e8b38b3c5a4b30d85613d8baf2cbed55fa
Andy Wright <atwright147@gmail.com>
2017-01-22 21:47:55 +0000

    addlogic to make tests pass

--- Person.js
+++ Person.js
@@ -3,10 +3,21 @@
 class Person {
 
 	age(dob) {
-		if (typeof dob !== 'string' && !dob.match(/\d{1,2}\/\d{1,2}\/(\d{4}|\d{2})/ig)) {
+		if (typeof dob === 'string' && !dob.match(/(\d{1,2})\/(\d{1,2})\/(\d{4}|\d{2})/ig)) {
 			return false;
 		}
-		return 38;
+		
+		const [dobDayStr, dobMonthStr, dobYearStr] = dob.split(/(?:\/|\.|\-)/ig);
+
+		const dobDay   = Number(dobDayStr);
+		const dobMonth = Number(dobMonthStr);
+		const dobYear  = Number(dobYearStr);
+
+		const dobObj = new Date(dobYear, dobMonth, dobDay);
+		const dobDiff = Date.now() - dobObj.getTime();
+		const diffObj = new Date(dobDiff);
+
+		return diffObj.getUTCFullYear() - 1970;
 	}
 }
 
