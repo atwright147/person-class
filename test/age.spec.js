39879fc3787fb34764366163901e60abf2ffb5d3
Andy Wright <atwright147@gmail.com>
2017-01-28 15:31:49 +0000

    add more date tests

--- test/age.spec.js
+++ test/age.spec.js
@@ -8,7 +8,7 @@ describe('Age', function() {
 	let clock;
 
 	beforeEach(() => {
-		Person = new PersonClass(2, 2, '22/09/1978', 'male', 6, 'British');
+		Person = new PersonClass(2, 2, '19/09/1980', 'male', 6, 'British');
 
 		const mockDate = new Date(2015, 8, 28);
 		clock = sinon.useFakeTimers(mockDate.getTime());
@@ -20,17 +20,29 @@ describe('Age', function() {
 
 	it('should return false for malformed dates', () => {
 		Person = new PersonClass(2, 2, null, 'male', 6, 'British');
-
 		expect(Person.age()).toBe(null);
+
+		Person = new PersonClass(2, 2, false, 'male', 6, 'British');
+		expect(Person.age()).toBe(null);
+
+		Person = new PersonClass(2, 2, 'abcdef', 'male', 6, 'British');
+		expect(Person.age()).toBe(null);
+
+		Person = new PersonClass(2, 2, 'aa/bb/cccc', 'male', 6, 'British');
+		expect(Person.age()).toBe(null);
+
+		Person = new PersonClass(2, 2, '22/09/cccc', 'male', 6, 'British');
+		expect(Person.age()).toBe(null);
+
 	});
 
-	it('should return 36 for 22/09/1978', () => {
-		expect(Person.age()).toEqual(36);
+	it('should return 36 for 19/09/1980', () => {
+		expect(Person.age()).toEqual(34);
 	});
 
 	it('should accept "/", "." and "-" as delimiters', () => {
-		expect(Person.age()).toEqual(36);
-		expect(Person.age()).toEqual(36);
-		expect(Person.age()).toEqual(36);
+		expect(Person.age()).toEqual(34);
+		expect(Person.age()).toEqual(34);
+		expect(Person.age()).toEqual(34);
 	});
 });
